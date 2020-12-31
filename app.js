9fecb272ebd665626957cf86af082fbe84dd6a92
Isaiah Anthony <isaiah.anthony99@gmail.com>
2020-12-30 22:20:59 -0800

    more functions

--- app.js
+++ app.js
@@ -5,57 +5,57 @@ const gl = require('./utils/License.js')
 
 
 
-let questions [
+let questions = [
   {
     type: 'input',
     name: 'name',
     message: 'What is the name of your project?'
-  }
+  },
   {
     type: 'input',
     name: 'description',
     message: 'How would you describe your project?'
-  }
+  },
   {
     type: 'input',
     name: 'installation',
     message: 'How would one go about installing your app?'
-  }
+  },
   {
     type:'input',
     name: 'table of contents',
     message: 'How would you like your table of contents to read?'
-  }
+  },
   {
     type:'input',
     name:'usage',
     message:'How does one use your app for themself?'
-  }
+  },
   {
     type:'input',
     name:'contributing',
     message:'What are the guidelines on others contributing to your project?'
-  }
+  },
   {
     type:'input',
     name:'tests',
     message:'How does one test for errors in this app?'
-  }
+  },
   {
     type:'input',
     name:'questions',
     message:'How does one reach you for questions about your app?'
-  }
+  },
   {
     type:'input',
     name:'username',
     message:'Please enter your github username.'
-  }
+  },
   {
     type:'input',
     name:'email',
     email:'Please enter your email.'
-  }
+  },
   {
     type:'list',
     name:'license',
@@ -63,3 +63,21 @@ let questions [
   }
 
 ]
+
+function writeToFile(fileName, data) {
+  gl.writeLicenseFile(data.license)
+  let readme = markdown(data)
+  fs.writeFile(fileName, readme, (err) => { if (err) { console.log(err) } })
+}
+
+function init() {
+  inquirer.prompt(questions)
+    .then((res) => {
+      writeToFile('./results/README.md', res)
+    })
+    .catch((err) => {
+      console.error(err)
+    })
+}
+
+init();
